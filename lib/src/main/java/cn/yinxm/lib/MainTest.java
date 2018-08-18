eb89757377062713b6166aa018d5a78c879b346e
h4x0r139 <h4x0r_001@163.com>
2018-08-19 00:47:21 +0800

    add submodule

--- lib/src/main/java/cn/yinxm/lib/MainTest.java
+++ lib/src/main/java/cn/yinxm/lib/MainTest.java
@@ -1,9 +1,6 @@
-/*
- * Copyright (C) 2018 Baidu, Inc. All Rights Reserved.
- */
 package cn.yinxm.lib;
 
-public class MyClass {
+public class MainTest {
 
     public static void main (String [] args) {
         System.out.println("test");
