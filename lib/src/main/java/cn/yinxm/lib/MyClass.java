871e4a00e0afb1b6abab41c9fe7bf083219c2141
h4x0r139 <h4x0r_001@163.com>
2018-08-18 22:54:11 +0800

    增加测试工程

---
+++ lib/src/main/java/cn/yinxm/lib/MyClass.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2018 Baidu, Inc. All Rights Reserved.
+ */
+package cn.yinxm.lib;
+
+public class MyClass {
+
+    public static void main (String [] args) {
+        System.out.println("test");
+    }
+}
