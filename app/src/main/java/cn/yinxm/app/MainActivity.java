a28d0c55bd847064d3035e9b4d60867f9060da88
h4x0r139 <h4x0r_001@163.com>
2018-08-19 13:18:00 +0800

    submodule 依赖

--- app/src/main/java/cn/yinxm/app/MainActivity.java
+++ app/src/main/java/cn/yinxm/app/MainActivity.java
@@ -4,11 +4,15 @@ package cn.yinxm.app;
 import android.app.Activity;
 import android.os.Bundle;
 
+import cn.yinxm.and_lib.AndLib;
+
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AndLib.test();
     }
 }
